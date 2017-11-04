48a939996456dcf3e7289b6cbdefc1afd1daba93
Taranack <Taranack@gmail.com>
2017-11-03 23:02:01 -0400

    Initial commit

---
+++ app/src/main/java/org/acm/njit/njitacm/MainActivity.java
@@ -0,0 +1,66 @@
+package org.acm.njit.njitacm;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class MainActivity extends FragmentActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_tutoring:
+                    return nextFragment(new TutoringFragment());
+                case R.id.navigation_sigs:
+                    return nextFragment(new SIGFragment());
+                case R.id.navigation_home:
+                    return nextFragment(new HomeFragment());
+                case R.id.navigation_social_media:
+                    return nextFragment(new SocialMediaFragment());
+                case R.id.navigation_contact_us:
+                    return nextFragment(new ContactUsFragment());
+            }
+
+            return false;
+        }
+    };
+
+
+    private boolean nextFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+        return true;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        // Create a new Fragment to be placed in the activity layout
+        HomeFragment firstFragment = new HomeFragment();
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, firstFragment).commit();
+
+
+        navigation.setSelectedItemId(R.id.navigation_home);
+
+    }
+}
---
+++ app/src/main/java/org/acm/njit/njitacm/SocialMediaFragment.java
@@ -0,0 +1,22 @@
+package org.acm.njit.njitacm;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SocialMediaFragment extends Fragment {
+
+    public SocialMediaFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_social_media, container, false);
+    }
+}
