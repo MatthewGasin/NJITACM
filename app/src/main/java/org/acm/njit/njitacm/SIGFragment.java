b6011132cec19871e838b233c9f4fafd85a3f253
Joseph Zeffiro <joezeffiro@gmail.com>
2017-11-20 15:04:53 -0500

    changing layout of fragment

--- app/src/main/java/org/acm/njit/njitacm/SIGFragment.java
+++ app/src/main/java/org/acm/njit/njitacm/SIGFragment.java
@@ -1,11 +1,18 @@
 package org.acm.njit.njitacm;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 
 public class SIGFragment extends Fragment {
@@ -19,9 +26,13 @@ public class SIGFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        View view = inflater.inflate(R.layout.fragment_sig, container, false);
+
+       // GridView gv =  getView().findViewById(R.id.SigGV);
+
+      //  gv.setAdapter(new ArrayAdapter<String>(this.getActivity(), R.layout.fragment_sig, gridViewNames));
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sig, container, false);
+        return view;
     }
 
 }
