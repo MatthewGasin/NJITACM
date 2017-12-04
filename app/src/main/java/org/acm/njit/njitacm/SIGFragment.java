fe450cc7eead719d0972aa8f5941b46c9314533f
Joseph Zeffiro <joezeffiro@gmail.com>
2017-12-04 12:50:13 -0500

    added placeholder images in xml layout and the template for the button implementation.

--- app/src/main/java/org/acm/njit/njitacm/SIGFragment.java
+++ app/src/main/java/org/acm/njit/njitacm/SIGFragment.java
@@ -10,14 +10,17 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
-public class SIGFragment extends Fragment {
+public class SIGFragment extends Fragment implements View.OnClickListener {
 
-    String[] gridViewNames = {"Android","Algorithm","Applied Alg.","Linux","Sound Design","Web Design"};
+    //String[] gridViewNames = {"Android","Algorithm","Applied Alg.","Linux","Sound Design","Web Design"};
+    ImageButton BTandroid,BTalg,BTAppAlg,BTLinux,BTWebDev,BTRevEng;
 
     public SIGFragment() {
         // Required empty public constructor
@@ -27,12 +30,50 @@ public class SIGFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sig, container, false);
+        BTandroid = (ImageButton) getActivity().findViewById(R.id.imageButton3);
+        BTandroid.setOnClickListener(this);
+        BTalg = (ImageButton) getActivity().findViewById(R.id.imageButton4);
+        BTalg.setOnClickListener(this);
+        BTAppAlg = (ImageButton) getActivity().findViewById(R.id.imageButton5);
+        BTAppAlg.setOnClickListener(this);
+        BTLinux = (ImageButton) getActivity().findViewById(R.id.imageButton6);
+        BTLinux.setOnClickListener(this);
+        BTWebDev = (ImageButton) getActivity().findViewById(R.id.imageButton7);
+        BTWebDev.setOnClickListener(this);
+        BTRevEng = (ImageButton) getActivity().findViewById(R.id.imageButton8);
+        BTRevEng.setOnClickListener(this);
 
        // GridView gv =  getView().findViewById(R.id.SigGV);
 
       //  gv.setAdapter(new ArrayAdapter<String>(this.getActivity(), R.layout.fragment_sig, gridViewNames));
         // Inflate the layout for this fragment
         return view;
+    }
+
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.imageButton3:
+                Toast.makeText(getActivity(), "Your toast message.",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.imageButton4:
+                break;
+
+            case R.id.imageButton5:
+                break;
+
+            case R.id.imageButton6:
+                break;
+
+            case R.id.imageButton7:
+                break;
+
+            case R.id.imageButton8:
+                break;
+        }
+
+
     }
 
 }
